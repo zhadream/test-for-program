eaaf89c13df570b75bafdfef5b40a0497c88a910
zhadream <674526380@qq.com>
2019-07-22 14:49:48 +0800

    test1

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,11 @@ class Main {
     public static void main(String[] args){
         String[] strs = new String[]{"adsf", "12346", "a45dfs2"};
         Stream stream = Stream.of(strs);
-//        System.out.println(stream.findFirst());
-//        System.out.println(stream.findAny());
-//        System.out.println(stream.sorted());
-//        System.out.println(stream.skip(1));
-//        System.out.println(stream.limit(1));
+        System.out.println(stream.findFirst());
+        System.out.println(stream.findAny());
+        System.out.println(stream.sorted());
+        System.out.println(stream.skip(1));
+        System.out.println(stream.limit(1));
 
     }
 
