a861d808a3e28895efb5ce852363aba775039303
zhadream <674526380@qq.com>
2019-07-22 14:47:24 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+class Main {
+
+    public static void main(String[] args){
+        String[] strs = new String[]{"adsf", "12346", "a45dfs2"};
+        Stream stream = Stream.of(strs);
+//        System.out.println(stream.findFirst());
+//        System.out.println(stream.findAny());
+//        System.out.println(stream.sorted());
+//        System.out.println(stream.skip(1));
+//        System.out.println(stream.limit(1));
+
+    }
+
+
+}
